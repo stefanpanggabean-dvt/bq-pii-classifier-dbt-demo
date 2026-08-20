4eeaea3d008ea3a3ba9cb7a41a2e5529b9694810
Stefan Batara Panggabean <stefan.panggabean@devoteam.com>
2026-08-20 11:02:30 +0200

    feat(dbt): point model to raw_sensitive_data and configure schema policy tags

--- models/sensitive_data_be.sql
+++ models/sensitive_data_be.sql
@@ -1,4 +1,6 @@
--- Mock model for testing column tagging
+-- SQL model mapping pre-dbt raw table to transformed destination table
 SELECT 
-  'user_123' AS user_id,
-  'alice@example.com' AS user_email
+  user_id,
+  user_email,
+  phone_no
+FROM `bq-dbt-tags-sandbox-01.test_dataset_be.raw_sensitive_data`
