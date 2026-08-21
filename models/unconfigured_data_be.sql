d671e15148ef23f440a7a6fe3ac4b7f3162c2c21
Stefan Batara Panggabean <stefan.panggabean@devoteam.com>
2026-08-21 12:17:32 +0200

    feat(dbt): add credit_card to sensitive_data_be and add unconfigured_data_be SQL model

--- models/unconfigured_data_be.sql
+++ models/unconfigured_data_be.sql
@@ -3,7 +3,5 @@
 -- SQL model mapping pre-dbt raw table to transformed destination table
 SELECT 
   user_id,
-  user_email,
-  phone_no,
-  credit_card
-FROM `bq-dbt-tags-sandbox-01.test_dataset_be.raw_sensitive_data`
+  payment_ref
+FROM `bq-dbt-tags-sandbox-01.test_dataset_be.raw_unconfigured_data`
